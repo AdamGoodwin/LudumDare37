1cc164d7e00cdf3de06c5f87f957392373045a4f
AdamGoodwin <Suji.kun@gmail.com>
2017-03-14 21:48:49 -0600

    Add files via upload

---
+++ Scripts/Controller.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Controller : MonoBehaviour
+{
+    public GameObject[] walls;
+
+    public bool hasWon = false;
+    public GameObject WinOrLose;
+
+    public bool fire = false;
+    public bool water = false;
+    public bool air = false;
+    public bool earth = false;
+
+    void Update()
+    {
+        //win condition find all the elements
+        if(fire && water && air && earth)
+        {
+            hasWon = true;
+        }
+
+        //loss condition get squished
+        if (walls[0].transform.position == walls[1].transform.position && walls[2].transform.position == walls[3].transform.position)
+        {
+            Text text = WinOrLose.GetComponent<Text>();
+            text.text = "You Lose!";
+        }
+    }
+
+    public void SetFire()
+    {
+        fire = true;
+    }
+
+    public void SetWater()
+    {
+        water = true;
+    }
+
+    public void SetAir()
+    {
+        air = true;
+    }
+
+    public void SetEarth()
+    {
+        earth = true;
+    }
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 5;
+    public float rotatespeed = 30;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.WakeUp();
+
+        float horizontal = Input.GetAxis("Horizontal") * rotatespeed * Time.deltaTime;
+        transform.Rotate(0, horizontal, 0);
+
+        float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.Translate(0, 0, vertical);
+    }
+}
