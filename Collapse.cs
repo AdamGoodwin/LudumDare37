16a1b813d21c9ac28df29f2533ac3f2be4fdface
AdamGoodwin <Suji.kun@gmail.com>
2017-03-14 21:45:43 -0600

    Add files via upload

---
+++ Collapse.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collapse : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    public Controller controller;
+
+    void FixedUpdate()
+    {
+        if(!controller.hasWon)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        }
+    }
+}
